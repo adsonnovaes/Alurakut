0d4c8795611cc3819a0155a80e9d1ee67c292e9c
adsonnovaes <adsonnovaes7@gmail.com>
2021-07-16 01:38:17 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,38 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(prosps) {
 
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${prosps.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${prosps.githubUser}`}>
+          @{prosps.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
+
     </Box>
   )
 }
 
 export default function Home() {
+  
   const githubUser = 'adsonnovaes';
+  const [comunidades, setComunidades ]= React.useState([{
+    id: new Date().toISOString(),
+    title: 'Eu Odeio Acordar Cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);;
+
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -22,6 +41,19 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho'
   ]
+
+  function handleCreateCummunity(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const comunidade = {
+      id: new Date().toISOString(),
+      title: formData.get('title'),
+      image: formData.get('image'),
+    }
+
+    setComunidades([...comunidades, comunidade])
+  }
 
   return (
     <>
@@ -37,7 +69,32 @@ export default function Home() {
               Bem Vindo(a)
             </h1>
 
-            <OrkutNostalgicIconSet/>
+            <OrkutNostalgicIconSet />
+          </Box>
+
+          <Box>
+            <h2 className="subTitle" >O que você deseja fazer?</h2>
+            <form onSubmit={handleCreateCummunity} >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da comunidade?"
+                  type="text" />
+              </div>
+
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+
+              <button type="submit" >
+                Criar Comunidade
+              </button>
+            </form>
           </Box>
         </div>
 
@@ -49,10 +106,28 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((itemAtual, index) => {
                 return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/users/${itemAtual}`}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+
+          <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">
+              Comunidades ({comunidades.length})
+            </h2>
+            <ul>
+              {comunidades.map((itemAtual, index) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
                     </a>
                   </li>
                 )
